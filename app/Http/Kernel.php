77d8f9703693edc30f41f23be80fad8b3912a170
libraroom <dickykutir1219@gmail.com>
2025-07-05 12:36:02 +0700

    feat: force https redirect in production via ForceHttps middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,10 +7,12 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\ForceHttps::class,
         \App\Http\Middleware\EncryptCookies::class,
     ];
 
